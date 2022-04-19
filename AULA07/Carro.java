07b5e4e9de8d8d8f2858d1254f007407179541aa
PATERA <103973993+Markinhooo@users.noreply.github.com>
2022-04-19 19:19:02 +0000

    Initial commit

---
+++ AULA07/Carro.java
@@ -0,0 +1,14 @@
+public class Carro {
+public String marca;
+    public String modelo;
+      public String cor;
+    public int ano;
+
+    void status(){
+      System.out.println("Informações do carro");
+        System.out.println("Marca: "+this.marca);
+          System.out.println("Modelo: "+this.modelo);
+            System.out.println("Cor: "+this.cor);
+              System.out.println("Ano: "+this.ano);
+      }
+} 
---
+++ AULA07/aula07.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    Carro objC = new Carro();
+    objC.marca="Fiat";
+      objC.modelo="Uno";
+      objC.cor="Preto";
+        objC.ano=2010;
+        objC.status();
+      }
+  }
